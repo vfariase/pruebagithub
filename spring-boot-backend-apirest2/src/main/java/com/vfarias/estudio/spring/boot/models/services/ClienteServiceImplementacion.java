64692dc39e931f02f355c40353d3b9d953da09cf
vfarias <e>
2020-04-01 15:34:16 -0300

    Se implementa método findAll(paginador)

--- spring-boot-backend-apirest2/src/main/java/com/vfarias/estudio/spring/boot/models/services/ClienteServiceImplementacion.java
+++ spring-boot-backend-apirest2/src/main/java/com/vfarias/estudio/spring/boot/models/services/ClienteServiceImplementacion.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,10 +21,17 @@ public class ClienteServiceImplementacion implements IClienteService{
 	@Transactional
 	@Override
 	public List<Cliente> findAll() {
-		
 		return clienteDAO.findAll();
 	}
-
+	
+	
+	@Transactional
+	@Override
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDAO.findAll(pageable);
+	}
+	
+	
 	@Transactional
 	@Override
 	public Cliente save(Cliente cliente) {
@@ -43,6 +52,8 @@ public class ClienteServiceImplementacion implements IClienteService{
 		// TODO Auto-generated method stub
 		return clienteDAO.findById(id).orElse(null);
 	}
+
+	
 	
 	
 	
