fba53008a983f43f4773f22e1232307ffea52ca6
vfarias <e>
2020-04-01 15:32:58 -0300

    Se agrega método que lista a todos los clientes a través de un paginador

--- spring-boot-backend-apirest2/src/main/java/com/vfarias/estudio/spring/boot/models/services/IClienteService.java
+++ spring-boot-backend-apirest2/src/main/java/com/vfarias/estudio/spring/boot/models/services/IClienteService.java
@@ -2,11 +2,15 @@ package com.vfarias.estudio.spring.boot.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.vfarias.estudio.spring.boot.models.entity.Cliente;
 
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	public Page<Cliente> findAll(Pageable pageable);
 	public Cliente save(Cliente cliente);
 	//public Cliente update(Cliente cliente);
 	public Cliente findById(Long id);
